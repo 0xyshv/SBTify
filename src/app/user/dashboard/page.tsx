e5bf1b45394cbd9e383563d696f86323bc636372
0xyshv <writetoyshv@gmail.com>
2024-04-28 19:27:47 +0530

    chore: configure imports in user route

--- src/app/user/dashboard/page.tsx
+++ src/app/user/dashboard/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Connected } from "@/components/connected";
+import { InfoCard } from "@/components/user/dashboard/info-card";
+import { InfoTable } from "@/components/user/dashboard/info-table";
+import UserNavbar from "@/components/user/navbar";
 
 const UserDashboard = () => {
   return (
-    <div>UserDashboard</div>
+    <>
+      <Connected>
+        <UserNavbar />
+        <div className="p-8 flex flex-col gap-4">
+          <InfoCard />
+          <InfoTable />
+        </div>
+      </Connected>
+    </>
   )
 }
 
--- src/app/user/verification/page.tsx
+++ src/app/user/verification/page.tsx
@@ -1,8 +1,41 @@
+"use client"
+
 import React from 'react'
+import { Connected } from "@/components/connected";
+import UserNavbar from "@/components/user/navbar";
+import VerificationTable from "@/components/user/verification/verification-table";
+import { useAccount } from "wagmi";
+// import { authorizedUserTokenContractConfig } from "@/lib/contracts";
+import { useRouter } from "next/navigation";
 
 const UserVerification = () => {
+  const { address } = useAccount();
+  const router = useRouter();
+
+  // const { error, isSuccess } = useContractRead({
+  //   ...authorizedUserTokenContractConfig,
+  //   functionName: "getVerifiedUserMetadata",
+  //   args: [address],
+  //   onSuccess: (data: any) => {
+  //     console.log("Queried Auth Token", data);
+  //     if (data?.userName === "" || !(data?.category == "individual")) {
+  //       router.push("/");
+  //     }
+  //   },
+  //   onError: (error: any) => {
+  //     console.error("Error querying Auth Token", error);
+  //   },
+  // });
+
   return (
-    <div>UserVerification</div>
+    <>
+      <Connected>
+        <UserNavbar />
+        <div className="p-8 flex flex-col gap-4">
+          <VerificationTable />
+        </div>
+      </Connected>
+    </>
   )
 }
 
