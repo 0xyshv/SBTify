94d0b13fe58be623ad7daecc3008c1fd78f23617
0xyshv <writetoyshv@gmail.com>
2024-04-29 19:04:52 +0530

    chore: add abi for contracts

---
+++ src/lib/contracts.ts
@@ -0,0 +1,6 @@
+import { authorizedUserTokenABI } from "./abi/authorizedUserTokenAbi";
+
+export const authorizedUserTokenContractConfig = {
+  address: "0x415c26A2686000E9D8Dc65e3282697f073A0e9EE",
+  abi: authorizedUserTokenABI,
+} as const;
